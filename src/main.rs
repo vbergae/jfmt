be4b5227bdde8540f3953a0d94823af680851772
victor <victor@cork.es>
2024-08-13 20:10:57 +0000

    Read raw json from stdin

--- src/main.rs
+++ src/main.rs
@@ -21,17 +21,50 @@ mod nodes;
 mod parser;
 
 fn main() {
-    let args = Cli::parse();
+    let command = Command::parse();
+    let json = &command.contents();
 
-    println!("{}", formatter::format(&args.json));
+    println!("{}", formatter::format(json));
 }
 
 use clap::Parser;
+use clap_stdin::FileOrStdin;
 
 #[derive(Parser)]
 #[command(name = env!("CARGO_PKG_NAME"))]
 #[command(about = env!("CARGO_PKG_DESCRIPTION"))]
 #[command(version = env!("CARGO_PKG_VERSION"))]
-struct Cli {
-    json: String,
+struct Command {
+    #[clap(default_value = "-")]
+    input: FileOrStdin,
+}
+
+impl Command {
+    fn contents(&self) -> String {
+        let input_clone = self.input.clone();
+        let contents = input_clone.contents();
+
+        contents.unwrap_or("".to_string())
+    }
+}
+
+#[cfg(test)]
+mod command_tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+
+    #[test]
+    fn test_file_or_stdin() {
+        let contents = "{\"foo\": \"bar\"}";
+        let temp_file_path = "temp_test_file.txt";
+        let mut temp_file = File::create(temp_file_path).unwrap();
+        writeln!(temp_file, "{contents}").unwrap();
+
+        let args = Command::parse_from(&["test", temp_file_path]);
+        let input = args.contents();
+        assert_eq!(input, contents);
+
+        std::fs::remove_file(temp_file_path).unwrap();
+    }
 }
